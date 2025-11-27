7371a93fe2a8e566be8afd7c7decfa93865f189d
Dominix446 <97578554+Dominix446@users.noreply.github.com>
2025-11-27 21:17:46 +0530

    Create Problem_6.java

---
+++ Problem_6.java
@@ -0,0 +1,8 @@
+public class Problem_6{
+    public static void main(String[] args){
+        long a=((100)*(201)*(101))/6;
+        a=Math.abs(a-5050*5050);
+        System.out.println(a);
+    }
+}
+//standard summation formulae used
