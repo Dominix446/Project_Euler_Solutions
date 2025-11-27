a3a91ba6c503e955ec723e7c1abf5f1bdf4bb05e
Dominix446 <97578554+Dominix446@users.noreply.github.com>
2025-11-27 23:01:23 +0530

    Create Problem_25.java

---
+++ Problem_25.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    double phi = (1 + Math.sqrt(5)) / 2.0;
+    double logF = 0.0;
+    int index = 1;//first 2 terms are equal
+    while (logF <= 999) {
+      logF += Math.log10(phi);//acutally there isa rounding error,but as the logfunction is forgiving,we get the correct ans easily
+      index++;
+    }
+    System.out.println(index);
+  }
+}
